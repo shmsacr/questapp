360f39d5c48b78b727c32e904963c9e455b108e3
shmsacr <shmsacr@gmail.com>
2023-08-08 14:59:19 +0300

    feat: add find by user id in repository

--- src/main/java/com/project/questapp/repos/PostRepository.java
+++ src/main/java/com/project/questapp/repos/PostRepository.java
@@ -3,5 +3,8 @@ package com.project.questapp.repos;
 import com.project.questapp.entities.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PostRepository extends JpaRepository<Post,Long> {
+    List<Post> findByUserId(Long userId);
 }
