c4ca27c4ac0f22146bf3741a4311ab7ce3492862
shmsacr <shmsacr@gmail.com>
2023-08-08 15:01:12 +0300

    feat: create post controller and add mappings

---
+++ src/main/java/com/project/questapp/controllers/PostController.java
@@ -0,0 +1,44 @@
+package com.project.questapp.controllers;
+
+import com.project.questapp.entities.Post;
+import com.project.questapp.requests.PostCreateRequest;
+import com.project.questapp.requests.PostUpdateRequest;
+import com.project.questapp.services.PostService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/posts")
+public class PostController {
+    private PostService postService;
+
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+
+    @GetMapping
+    public List<Post> getAllPosts(@RequestParam Optional<Long> userId){
+    //requesparam aldığı parametreyi optinal yani isteğe bağlı yapar gelip gelmemesi zorunlu değildir
+        return postService.getAllPost(userId);
+
+    }
+    @PostMapping
+    public Post createOnePost(@RequestBody PostCreateRequest newPostCreate){
+        return postService.createOnePost(newPostCreate);
+    }
+    @GetMapping("/{postId}")
+    public Post getOnePost(@PathVariable Long postId){
+        return postService.getOnePostById(postId);
+    }
+    @PutMapping("/{postId}")
+    public Post updataOnePost(@PathVariable Long postId, @RequestBody PostUpdateRequest updatePost){
+        return postService.updateOnePost(postId,updatePost);
+    }
+    @DeleteMapping("/{postId}")
+    public void deleteOnePost(@PathVariable Long postId){
+        postService.deleteOnePost(postId);
+    }
+
+}
