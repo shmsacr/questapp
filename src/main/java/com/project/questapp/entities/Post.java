8714dc98e345840378c3914d64933e888570c1a6
shmsacr <shmsacr@gmail.com>
2023-08-08 14:58:26 +0300

    feat: add post relationship to user

--- src/main/java/com/project/questapp/entities/Post.java
+++ src/main/java/com/project/questapp/entities/Post.java
@@ -1,7 +1,10 @@
 package com.project.questapp.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Data;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 @Entity
 @Table(name ="post")
@@ -9,7 +12,11 @@ import lombok.Data;
 public class Post {
     @Id
     Long id;
-    Long userId;
+    @ManyToOne(fetch = FetchType.LAZY)//user objesini hemen çekme postu çektiğimizde user gelmesin
+    @JoinColumn(name = "user_id",nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE) //Kullanıcı silinirse o kullanıcının bütün postları da silinsin
+    @JsonIgnore
+    User user;
     String title;
     @Lob
     @Column(columnDefinition = "text")
