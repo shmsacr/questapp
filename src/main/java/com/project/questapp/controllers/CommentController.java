11040824a3fe4c2dc59022e626a017a6d01a86a6
shmsacr <shmsacr@gmail.com>
2023-08-09 17:17:00 +0300

    feat: create comment controller and all method

---
+++ src/main/java/com/project/questapp/controllers/CommentController.java
@@ -0,0 +1,41 @@
+package com.project.questapp.controllers;
+
+import com.project.questapp.entities.Comment;
+import com.project.questapp.requests.CommentCreateRequest;
+import com.project.questapp.requests.CommentUpdataRequest;
+import com.project.questapp.services.CommentService;
+import org.springframework.web.bind.annotation.*;
+import org.yaml.snakeyaml.tokens.CommentToken;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/comments")
+public class CommentController {
+    private CommentService commentService;
+
+    public CommentController(CommentService commentService) {
+        this.commentService = commentService;
+    }
+    @GetMapping
+    public List<Comment> getAllComments(@RequestParam Optional<Long> userId,@RequestParam Optional<Long> postId){
+        return commentService.getALlCommentsWithParam(userId,postId);
+    }
+    @GetMapping("/{commentId}")
+    public Comment getOneCommet(@PathVariable Long commentId){
+        return commentService.getOneCommentById(commentId);
+    }
+    @PostMapping
+    public Comment createOneComment(@RequestBody CommentCreateRequest request){
+        return commentService.createOneComment(request);
+    }
+    @PutMapping("/{commetId}")
+    public Comment updataOnePost(@PathVariable Long commentId, @RequestBody CommentUpdataRequest updataComment){
+        return commentService.updataOneComment(commentId,updataComment);
+    }
+    @DeleteMapping("/{commetId}")
+    public void deleteOnePost(@PathVariable Long commentId){
+        commentService.deleteOneComment(commentId);
+    }
+}
