cd321542aed3d4c2517ad6ca9aa74761955e67a6
shmsacr <shmsacr@gmail.com>
2023-08-10 17:45:47 +0300

    feat: delete old like repository

--- src/main/java/com/project/questapp/repos/LikeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.project.questapp.repos;
-
-import com.project.questapp.entities.Like;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LikeRepository extends JpaRepository<Like,Long>{
-}
