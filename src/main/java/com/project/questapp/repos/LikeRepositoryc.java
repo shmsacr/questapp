9478bffa02ec7d70cd51da03d4b966cfccd1b881
shmsacr <shmsacr@gmail.com>
2023-08-10 17:44:49 +0300

    feat: create like repository

---
+++ src/main/java/com/project/questapp/repos/LikeRepositoryc.java
@@ -0,0 +1,14 @@
+package com.project.questapp.repos;
+
+import com.project.questapp.entities.Like;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface LikeRepositoryc extends JpaRepository<Like,Long> {
+    List<Like> findByUserIdAndPostId(Long userId, Long postId);
+
+    List<Like> findByUserId(Long userId);
+
+    List<Like> findByPostId(Long postId);
+}
