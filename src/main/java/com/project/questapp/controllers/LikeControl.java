82c34982a70159512b0d1729aab26fc30b7d3f23
shmsacr <shmsacr@gmail.com>
2023-08-14 21:09:43 +0300

    feat: add like to likeResponse

--- src/main/java/com/project/questapp/controllers/LikeControl.java
+++ src/main/java/com/project/questapp/controllers/LikeControl.java
@@ -2,6 +2,7 @@ package com.project.questapp.controllers;
 
 import com.project.questapp.entities.Like;
 import com.project.questapp.requests.LikeCreateRequest;
+import com.project.questapp.response.LikeResponse;
 import com.project.questapp.services.LikeServices;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,7 +22,7 @@ public class LikeControl {
         return likeServices.getOneLikeById(likeId);
     }
     @GetMapping
-    public List<Like> getAllLikes(@RequestParam Optional<Long> userId,@RequestParam Optional<Long> postId){
+    public List<LikeResponse> getAllLikes(@RequestParam Optional<Long> userId, @RequestParam Optional<Long> postId){
         return likeServices.getAllLikeWithParam(userId,postId);
     }
 
