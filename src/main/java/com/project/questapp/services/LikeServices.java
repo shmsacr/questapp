c166ef46afd327b5fd121d32f8fe5d4a4bc9a7c6
shmsacr <shmsacr@gmail.com>
2023-08-14 22:12:10 +0300

    feat: create setter post service and like to likeresponse

--- src/main/java/com/project/questapp/services/LikeServices.java
+++ src/main/java/com/project/questapp/services/LikeServices.java
@@ -5,35 +5,46 @@ import com.project.questapp.entities.Post;
 import com.project.questapp.entities.User;
 import com.project.questapp.repos.LikeRepositoryc;
 import com.project.questapp.requests.LikeCreateRequest;
+import com.project.questapp.response.LikeResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class LikeServices {
     private LikeRepositoryc likeRepositoryc;
     private UserService userService;
+
     private PostService postService;
 
-    public LikeServices(LikeRepositoryc likeRepositoryc, UserService userService, PostService postService) {
+    @Autowired
+    public void setPostService(PostService postService){
+        this.postService = postService;
+    }
+    public LikeServices(LikeRepositoryc likeRepositoryc, UserService userService ) {
         this.likeRepositoryc = likeRepositoryc;
         this.userService = userService;
-        this.postService = postService;
     }
 
     public Like getOneLikeById(Long likeId) {
         return likeRepositoryc.findById(likeId).orElse(null);
     }
 
-    public List<Like> getAllLikeWithParam(Optional<Long> userId, Optional<Long> postId) {
+    public List<LikeResponse> getAllLikeWithParam(Optional<Long> userId, Optional<Long> postId) {
+        List<Like> list;
         if (userId.isPresent() && postId.isPresent()){
-            return likeRepositoryc.findByUserIdAndPostId(userId.get(),postId.get());
+            list = likeRepositoryc.findByUserIdAndPostId(userId.get(),postId.get());
         } else if (userId.isPresent()) {
-            return likeRepositoryc.findByUserId(userId.get());
+            list =  likeRepositoryc.findByUserId(userId.get());
         } else if (postId.isPresent()) {
-            return likeRepositoryc.findByPostId(postId.get());
-        }else return likeRepositoryc.findAll();
+            list =  likeRepositoryc.findByPostId(postId.get());
+        }else list =  likeRepositoryc.findAll();
+
+        return list.stream().map(like -> new LikeResponse(like)).collect(Collectors.toList());
     }
 
         public Like createOneLike(LikeCreateRequest request) {
