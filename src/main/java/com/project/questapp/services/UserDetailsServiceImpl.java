89a1bc6e8ee444c370df2f79c89ee88405e5206c
shmsacr <shmsacr@gmail.com>
2023-08-15 22:16:06 +0300

    feat: create user details service

---
+++ src/main/java/com/project/questapp/services/UserDetailsServiceImpl.java
@@ -0,0 +1,30 @@
+package com.project.questapp.services;
+
+import com.project.questapp.entities.User;
+import com.project.questapp.repos.UserRepository;
+import com.project.questapp.security.JwtUserDetails;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUserName(username);
+
+        return JwtUserDetails.create(user);
+    }
+
+    public UserDetails loadUserById(Long id){
+         User user = userRepository.findById(id).get();
+         return JwtUserDetails.create(user);
+    }
+}
